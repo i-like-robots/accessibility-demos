e5abab9b04c2adbd617eec02d677a609b4d83344
Matt Hinchliffe <matt@maketea.co.uk>
2014-10-27 00:22:59 +0000

    Control focus when opening and closing modal window

--- modal-window/modal.js
+++ modal-window/modal.js
@@ -9,6 +9,8 @@ Modal.prototype.init = function() {
     this.modalOverlay.className = 'modal-overlay';
 
     this.modalWindow = document.createElement('div');
+    this.modalWindow.setAttribute('role', 'dialog');
+    this.modalWindow.setAttribute('tabindex', '0');
     this.modalWindow.className = 'modal';
 
     this.modalWrapper = document.createElement('div');
@@ -24,6 +26,19 @@ Modal.prototype.init = function() {
 
     this.closeButton.onclick = this.close.bind(this);
 
+    this.escapeHandler = function(e) {
+        if (e.keyCode === 27) this.close();
+    }.bind(this);
+
+    this.focusHandler = function(e) {
+        var target = e.relatedTarget || e.target;
+
+        if (!this.modalWindow.contains(target)) {
+            e.preventDefault();
+            this.modalWindow.focus();
+        }
+    }.bind(this);
+
     this.modalWindow.appendChild(this.modalWrapper);
     this.modalWrapper.appendChild(this.modalContent);
     this.modalWindow.appendChild(this.closeButton);
@@ -31,13 +46,25 @@ Modal.prototype.init = function() {
     this.isOpen = false;
 };
 
-Modal.prototype.open = function(content) {
+Modal.prototype.open = function(content, title) {
     if (this.isOpen) return;
+
+    this.trigger = document.activeElement;
+
+    this.modalWindow.setAttribute('aria-label', title);
 
     this.modalContent.innerHTML = content;
 
     this.target.appendChild(this.modalOverlay);
     this.target.appendChild(this.modalWindow);
+
+    this.modalWindow.focus();
+
+    window.addEventListener('keyup', this.escapeHandler, false);
+    // Firefox doesn't support focusin or focusout
+    // https://bugzilla.mozilla.org/show_bug.cgi?id=687787
+    window.addEventListener('focusin', this.focusHandler, false);
+    window.addEventListener('focusout', this.focusHandler, false);
 
     this.isOpen = true;
 };
@@ -45,6 +72,12 @@ Modal.prototype.open = function(content) {
 Modal.prototype.close = function() {
     this.target.removeChild(this.modalOverlay);
     this.target.removeChild(this.modalWindow);
+
+    window.removeEventListener('keyup', this.escapeHandler, false);
+    window.removeEventListener('focusin', this.focusHandler, false);
+    window.removeEventListener('focusout', this.focusHandler, false);
+
+    this.trigger.focus();
 
     this.isOpen = false;
 };
